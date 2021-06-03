0659f7e450614ab04c2711d97cf977cb582dc4e1
Arpitha S Kori <arpitha.ssaa@gmail.com>
2021-06-04 00:20:43 +0530

    Activity 1 Question 1 commit

---
+++ Java_Activity/src/EmployeeMain.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the Employee details");
+		System.out.println("Enter the name");
+		e.setName(scan.next());
+		System.out.println("Enter Address");
+		e.setAddress(scan.next());
+		System.out.println("Enter Mobile");
+		e.setMobile(scan.next());
+		System.out.println("Employee Details");
+		System.out.println("Name: "+e.getName());
+		System.out.println("Address: "+e.getAddress());
+		System.out.println("Mobile: "+e.getMobile());
+	}
+
+}
